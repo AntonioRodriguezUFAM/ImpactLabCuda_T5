006aa188f4930e12397d02ae44948d425b3da53e
AntonioRodriguezUFAM <antonio.rodrigue@icomp.ufam.edu.br>
2025-07-22 18:11:19 -0400

    Add Cuda Threads Idx

--- main.cu
+++ main.cu
@@ -11,6 +11,9 @@ __global__ void mykernel(void) {
 
 	//std::cout << "Hello Wold CUDA !\n";
 	printf("Hello from GPU (CUDA Kernel)!\n");
+	printf("Hello from CUDA thread X %d\n", threadIdx.x);
+	printf("Hello from CUDA thread Y %d\n", threadIdx.y);
+	printf("Hello from CUDA thread Z %d\n", threadIdx.z);
 }
 
 
@@ -28,7 +31,8 @@ int main(int argc, char** argv) {
 
 	// Step 04 : Call “kernel” routine to execute on GPU(with CUDA syntax that defines no of threads and their physical structure)
 	std::cout << "Hello Wold From CPU !\n";
-	mykernel << <1, 1 >> > ();
+	mykernel << <1, 10 >> > ();
+
 		
 	printf("Hello from GPU (CPU Main)!\n");
 	// Step 05 : Transfer results from GPU to CPU
